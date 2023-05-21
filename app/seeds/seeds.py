61b4e34a650b3ae257ab023e1775454c813e080e
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-05-20 20:34:42 -0500

    added seed data

--- app/seeds/seeds.py
+++ app/seeds/seeds.py
@@ -49,20 +49,59 @@ def seed_database():
         developer='Gizzard',
         publisher='UntetheredEdge Interactive',
         price=29.99,
-        genre='First-Person Shooter',
+        genre='Multiplayer Shooter',
         is_promoted=True,
         is_on_sale=False
     )
 
     game3 = Game (
-        name='Ogrewatch',
-        description='This shooter will give you a shrekking good time.',
-        release_date=date(2020, 3, 17),
-        creator_id=2,
-        developer='Gizzard',
+        name='Halo Kitty',
+        description='When kitties have the ability to shape the planet',
+        release_date=date(2010, 3, 17),
+        creator_id=3,
+        developer='YBox Game Studios',
         publisher='UntetheredEdge Interactive',
-        price=29.99,
+        price=49.99,
         genre='First-Person Shooter',
+        is_promoted=True,
+        is_on_sale=False
+    )
+
+    game4 = Game (
+        name='Ruined friendships 3',
+        description='You thought you had a party, but the party came to you',
+        release_date=date(2005, 5, 13),
+        creator_id=4,
+        developer='Litendo',
+        publisher='UntetheredEdge Interactive',
+        price=109.99,
+        genre='Party',
+        is_promoted=True,
+        is_on_sale=False
+    )
+
+    game5 = Game (
+        name='Resident Smeagol',
+        description='My preciouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuus',
+        release_date=date(2009, 9, 12),
+        creator_id=3,
+        developer='Nocapcom',
+        publisher='UntetheredEdge Interactive',
+        price=11.11,
+        genre='Horror',
+        is_promoted=True,
+        is_on_sale=False
+    )
+
+    game6 = Game (
+        name='Retail 13 Manager',
+        description="Just when you thought you couldn't simulate working under your old boss",
+        release_date=date(2018, 12, 25),
+        creator_id=3,
+        developer='Circle enix',
+        publisher='UntetheredEdge Interactive',
+        price=299.99,
+        genre='Life Simulator',
         is_promoted=True,
         is_on_sale=False
     )
@@ -105,29 +144,75 @@ def seed_database():
 
     game2_image_2 = GameImage (
         game_id=2,
-        url='https://www.denofgeek.com/wp-content/uploads/2022/04/Overwatch-2.jpg?fit=3840%2C2160',
+        url='https://bloximages.newyork1.vip.townnews.com/dailyemerald.com/content/tncms/assets/v3/editorial/8/73/87346b56-0210-53c0-9c3d-ef62ec11ed2c/5ba530ba6db24.image.jpg?resize=889%2C500',
         preview=False
     )
 
     game2_image_3 = GameImage (
         game_id=2,
-        url='https://www.denofgeek.com/wp-content/uploads/2022/04/Overwatch-2.jpg?fit=3840%2C2160',
+        url='https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iuHoL7F21xko/v1/-1x-1.jpg',
         preview=False
     )
 
     game2_image_4 = GameImage (
         game_id=2,
-        url='https://www.denofgeek.com/wp-content/uploads/2022/04/Overwatch-2.jpg?fit=3840%2C2160',
+        url='https://cdn.tempostorm.com/articles/multigenre0.large.jpg',
         preview=False
     )
 
     game2_image_5 = GameImage (
         game_id=2,
-        url='https://www.denofgeek.com/wp-content/uploads/2022/04/Overwatch-2.jpg?fit=3840%2C2160',
+        url='https://i.ytimg.com/vi/o52PDBdWwdo/maxresdefault.jpg',
         preview=False
     )
 
+    game3_image_preview = GameImage (
+        game_id=3,
+        url='https://external-preview.redd.it/JhKaZ9TrokOq8JEiSrEqHLmDJEImCLiNBeDS4lwkP3c.jpg?auto=webp&s=76bef8dde8afc7098312d2d2513be564ecd4be00',
+        preview=True
+    )
 
+    game3_image_2 = GameImage (
+        game_id=3,
+        url='https://cdn.vox-cdn.com/thumbor/rLO1f9sCiD47DuboyPP6WUyzkRA=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/23120097/Screenshot__634_.png',
+        preview=False
+    )
+
+    game3_image_3 = GameImage (
+        game_id=3,
+        url='https://64.media.tumblr.com/58f5cda3d5c6a96402e175b49d2a31d7/tumblr_p5tlvb91MP1tdtz92o1_1280.png',
+        preview=False
+    )
+
+    game3_image_4 = GameImage (
+        game_id=3,
+        url='https://live.staticflickr.com/2922/14853210513_de48c0caaf_b.jpg',
+        preview=False
+    )
+
+    game3_image_5 = GameImage (
+        game_id=3,
+        url='https://www.thetechgame.com/images/news/article/df5abc4c62.jpg',
+        preview=False
+    )
+
+    game4_image_preview = GameImage (
+        game_id=4,
+        url='https://laughingsquid.com/wp-content/uploads/2014/06/10357642_1488398131374574_8483691325398290806_o.jpg',
+        preview=True
+    )
+
+    game5_image_preview = GameImage (
+        game_id=5,
+        url='https://imgix.ranker.com/user_node_img/50058/1001143032/original/g-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375',
+        preview=True
+    )
+
+    game6_image_preview = GameImage (
+        game_id=6,
+        url='https://m0.her.ie/wp-content/uploads/2013/12/20131212-16074213-4.jpg',
+        preview=True
+    )
 
     # Add order instances to the session
 
