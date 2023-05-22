aba6ed5ab86a17e1544df2480492882a3b0a76fd
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-05-22 01:24:11 -0500

    finished comments reducer draft

---
+++ react-app/src/store/comments.js
@@ -0,0 +1,87 @@
+import { getAllGamesThunk } from "./games"
+
+const GET_ALLCOMMENTS = "comments/GET_ALLCOMMENTS"
+const POST_COMMENT = "comments/POST_COMMENT"
+const DELETE_COMMENT = "comments/DELETE_COMMENT"
+
+export const getAllCommentsAction = (allComments) => ({
+    type: GET_ALLCOMMENTS,
+    allComments
+})
+
+export const postCommentAction = (userInput) => ({
+    type: POST_COMMENT,
+    userInput
+})
+
+export const deleteCommentAction = (input) => ({
+    type: DELETE_COMMENT,
+    input
+})
+
+export const getAllCommentsThunk = (gameId) => async (dispatch) => {
+    const res = await fetch(`/api/games/${gameId}/comments`)
+
+    if (res.ok) {
+        const allGameComments = await res.json()
+        dispatch(getAllCommentsAction(allGameComments))
+        return allGameComments
+    }
+}
+
+export const postCommentThunk = (commentInput) => async (dispatch) => {
+    // const { game_id, comment } = commentInput
+    const res = await fetch(`/api/games/${commentInput.game_id}/comments`, {
+        method: 'POST',
+        body: commentInput
+    })
+
+    if (res.ok) {
+        const commentPost = await res.json()
+        dispatch(postCommentAction(commentPost))
+    }
+}
+
+export const deleteCommentThunk = (commentId) => async (dispatch) => {
+    const res = await fetch(`/api/comments/${commentId}`, {
+        method: 'DELETE'
+    })
+
+    if (res.ok) {
+        const deletedComment = await res.json()
+        dispatch(deleteCommentAction(commentId))
+        return deletedComment
+    }
+}
+
+const initialState = {
+    game: {},
+    user: {}
+}
+
+export default function commentsReducer(state = initialState, action) {
+    let newCommentsState;
+    const normalizerFunction = (actionData, nestedObjInState) => {
+        actionData.forEach(comment => {
+            nestedObjInState[comment.id] = comment
+        })
+    }
+	switch (action.type) {
+        case GET_ALLCOMMENTS:
+            newCommentsState = { ...state, game: {} }
+            normalizerFunction((action.allComments.comments), (newCommentsState.game))
+            return newCommentsState
+        case POST_COMMENT:
+            newCommentsState = { ...state, game: { ...state.game }, user: { ...state.user } }
+            newCommentsState.game = action.userInput
+            newCommentsState.user = action.userInput
+            return newCommentsState
+        case DELETE_COMMENT:
+            newCommentsState = { ...state, game: { ...state.game }, user: { ...state.user } }
+            delete newCommentsState.game[action.input]
+            delete newCommentsState.user[action.input]
+            return newCommentsState
+		default:
+			return state;
+	}
+}
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import gamesReducer from './games';
+import commentsReducer from './comments';
 
 const rootReducer = combineReducers({
   session,
-  games: gamesReducer
+  games: gamesReducer,
+  comments: commentsReducer
 });
 
 
