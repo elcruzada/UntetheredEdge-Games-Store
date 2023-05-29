bb8fddcc11ea61f962bd3984631e7188f40b5d2d
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-05-28 22:47:50 -0500

    navigation

--- react-app/src/components/GamesPages/BrowseGamesPages.js
+++ react-app/src/components/GamesPages/BrowseGamesPages.js
@@ -4,6 +4,7 @@ import { getAllCommentsThunk } from "../../store/comments"
 import { useHistory } from "react-router-dom"
 import BrowseGamesPagesCard from "../UI/BrowseGamesPagesCard"
 import { getAllGamesThunk } from "../../store/games"
+import './BrowseGamesPages.css'
 
 
 const BrowseGamesPages = () => {
@@ -14,7 +15,16 @@ const BrowseGamesPages = () => {
 
     useEffect(() => {
         dispatch(getAllGamesThunk())
-    },[dispatch])
+    }, [dispatch])
+
+    const cartRedirectHandler = () => {
+        if (!sessionUser) {
+            window.alert("You must be logged in to access your cart")
+        } else {
+            history.push('/cart')
+        }
+    }
+
 
     if (!allGames) return null
 
@@ -24,12 +34,45 @@ const BrowseGamesPages = () => {
     return (
         <div className='browse-games-pages-outer'>
             <div className='browse-games-pages-inner'>
-            {convertedGames && convertedGames.map(game => {
-                // console.log('GAAAEM', game)
-                return <BrowseGamesPagesCard
-                key={game.id}
-                game={game}/>
-            })}
+                <p
+                    style={{ color: 'white', paddingBottom: '3rem', display: 'flex', alignItems: 'center' }}
+                >A gaming marketplace where you can buy games, leave your thoughts, and publish your own!</p>
+                <div className="top-bar-homepage-container">
+                    <div className="top-bar-homepage-container-left-column">
+                        <div className="search-bar">
+                            <input type="text" placeholder="Feature coming soon" className="search-input" />
+                            <button className="search-button"
+                                onClick={() => window.alert("Feature coming soon")}
+                            >
+                                <i className="fas fa-search"></i>
+                            </button>
+                        </div>
+                        <h2
+                            style={{ color: 'white', fontSize: '16px', fontWeight: 'light', width: '6rem', textAlign: 'center', borderRadius: '10px', padding: ".5rem", cursor: 'pointer' }}
+                            onClick={() => history.push(`/`)}
+                        >Discover</h2>
+                        <h2
+                            style={{ color: 'white', fontSize: '16px', fontWeight: 'light', width: '6rem', textAlign: 'center', borderRadius: '10px', padding: ".5rem", cursor: 'pointer' }}
+                            onClick={() => history.push(`/games/browse`)}
+                        >Browse</h2>
+                        <h2
+                            style={{ color: 'white', fontSize: '16px', fontWeight: 'light', width: '6rem', textAlign: 'center', borderRadius: '10px', padding: ".5rem", cursor: 'pointer' }}
+                            onClick={() => window.alert("Feature coming soon")}
+                        >News</h2>
+                    </div>
+                    <div className="top-bar-homepage-container-right-column">
+                        <h2
+                            style={{ color: 'white', fontSize: '16px', fontWeight: 'light', width: '5rem', textAlign: 'center', borderRadius: '10px', padding: ".5rem", cursor: 'pointer' }}
+                            onClick={cartRedirectHandler}
+                        >Cart</h2>
+                    </div>
+                </div>
+                {convertedGames && convertedGames.map(game => {
+                    // console.log('GAAAEM', game)
+                    return <BrowseGamesPagesCard
+                        key={game.id}
+                        game={game} />
+                })}
             </div>
         </div>
     )
--- react-app/src/components/GamesPages/Homepage.js
+++ react-app/src/components/GamesPages/Homepage.js
@@ -37,24 +37,38 @@ const Homepage = () => {
         <>
             <div className="global-outer-container">
                 <div className="global-inner-container">
-                    <div className="top-bar-homepage-container">
                         <p
                             style={{ color: 'white', paddingBottom: '3rem', display: 'flex', alignItems: 'center' }}
-                        >A gaming marketplace where you can buy games, leave your thoughts on them, and even publish your own!</p>
+                        >A gaming marketplace where you can buy games, leave your thoughts, and publish your own!</p>
+                    <div className="top-bar-homepage-container">
+                        <div className="top-bar-homepage-container-left-column">
                         <div className="search-bar">
                             <input type="text" placeholder="Feature coming soon" className="search-input" />
-                            <button className="search-button">
+                            <button className="search-button"
+                            onClick={() => window.alert("Feature coming soon")}
+                            >
                                 <i className="fas fa-search"></i>
                             </button>
                         </div>
                         <h2
                             style={{ color: 'white', fontSize:'16px', fontWeight: 'light', width: '6rem', textAlign: 'center', borderRadius: '10px', padding: ".5rem", cursor: 'pointer' }}
+                            onClick={() => history.push(`/`)}
+                        >Discover</h2>
+                        <h2
+                            style={{ color: 'white', fontSize:'16px', fontWeight: 'light', width: '6rem', textAlign: 'center', borderRadius: '10px', padding: ".5rem", cursor: 'pointer' }}
                             onClick={() => history.push(`/games/browse`)}
                         >Browse</h2>
                         <h2
-                            style={{ color: 'white', fontSize:'16px', fontWeight: 'light', width: '4rem', textAlign: 'center', borderRadius: '10px', padding: ".5rem", cursor: 'pointer' }}
+                            style={{ color: 'white', fontSize:'16px', fontWeight: 'light', width: '6rem', textAlign: 'center', borderRadius: '10px', padding: ".5rem", cursor: 'pointer' }}
+                            onClick={() => window.alert("Feature coming soon")}
+                        >News</h2>
+                        </div>
+                        <div className="top-bar-homepage-container-right-column">
+                        <h2
+                            style={{ color: 'white', fontSize:'16px', fontWeight: 'light', width: '5rem', textAlign: 'center', borderRadius: '10px', padding: ".5rem", cursor: 'pointer' }}
                             onClick={cartRedirectHandler}
                         >Cart</h2>
+                        </div>
                     </div>
                     <HomepageFeaturedCard allGames={allGames} />
 
