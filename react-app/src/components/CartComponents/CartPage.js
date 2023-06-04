2c9037f5d8225317ec0f7e7e8cb9764b3323b0b9
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-06-04 16:50:47 -0500

    loading screens implemented

--- react-app/src/components/CartComponents/CartPage.js
+++ react-app/src/components/CartComponents/CartPage.js
@@ -5,6 +5,7 @@ import CartGamesCard from '../UI/CartGamesCard'
 import { useState, useEffect } from 'react'
 import { deleteUserCartThunk, getUserCartThunk } from '../../store/cart'
 import LowerNavBar from '../LowerNavBar/LowerNavBar'
+import LoadingScreen from '../UI/Loading/LoadingScreen'
 
 const CartPage = () => {
     const dispatch = useDispatch()
@@ -17,89 +18,96 @@ const CartPage = () => {
     // console.log('CAART', cart)
     // const cartCopy = {...cart}
     const [total, setTotal] = useState(0)
+    const [loading, setLoading] = useState(true);
     // console.log('CAAART', cart)
     // const totalCost = cart.forEach(game => {
-        //     total += game.price
-        // })
+    //     total += game.price
+    // })
     const removeFromCartHandler = async (gameId) => {
         await dispatch(deleteUserCartThunk(gameId))
         await dispatch(getUserCartThunk())
         history.push('/cart')
     }
 
-        useEffect(() => {
-            let newTotal = 0
-            if (cart) {
-                cart.forEach(game => {
-                    newTotal += game.price
-                })
+    useEffect(() => {
+        let newTotal = 0
+        if (cart) {
+            cart.forEach(game => {
+                newTotal += game.price
+            })
+        }
+        setTotal(newTotal)
+    }, [cart])
+
+    useEffect(() => {
+        dispatch(getUserCartThunk()).then(() => {
+            setLoading(false);
+        })
+    }, [dispatch])
+
+    const cartCheckoutHandler = async () => {
+        try {
+            const res = await fetch(`/api/cart/order`, {
+                method: 'POST'
+            })
+            if (res.ok) {
+                const newCart = await res.json()
             }
-            setTotal(newTotal)
-        },[cart])
-
-        useEffect(() => {
-            dispatch(getUserCartThunk())
-        },[dispatch])
-
-        const cartCheckoutHandler = async () => {
-            try {
-                const res = await fetch(`/api/cart/order`, {
-                    method: 'POST'
-                })
-                if (res.ok) {
-                    const newCart = await res.json()
-                }
-            } catch (error) {
-                console.error('Cart error', error)
-            }
-
-            // setTimeout(() => {
-
-            //     history.push('/profile');
-
-            // }, 2000)
-            // history.push('/games')
-            history.push('/profile')
+        } catch (error) {
+            console.error('Cart error', error)
         }
 
-        // const cartCheckoutHandler = () => {
-        //     window.alert("Feature coming soon!")
-        // }
-        // if (!cart) return null
+        // setTimeout(() => {
+
+        //     history.push('/profile');
+
+        // }, 2000)
+        // history.push('/games')
+        history.push('/profile')
+    }
+
+    // const cartCheckoutHandler = () => {
+    //     window.alert("Feature coming soon!")
+    // }
+    // if (!cart) return null
     return (
+        <>
+            {
+                loading ? <LoadingScreen /> :
+                    <div className='cart-page-outer-wrapper'>
+                        <div className='cart-page-inner-wrapper'>
 
+                            <LowerNavBar />
 
-            <div className='cart-page-outer-wrapper'>
-                <div className='cart-page-inner-wrapper'>
-
-                    <LowerNavBar />
-
-                    <h1 style={{color: 'white', marginTop: '3rem', marginBottom: '3rem'}}>My Cart</h1>
-                    <div className='cart-page-checkout'>
-                        <h1>Games Summary</h1>
-                        <p>Total {total} </p>
-                        <button
-                        onClick={cartCheckoutHandler}
-                        >Checkout</button>
+                            <h1 style={{ color: 'white', marginTop: '3rem', marginBottom: '3rem' }}>My Cart</h1>
+                            <div className='cart-page-checkout'>
+                                <h1>Games Summary</h1>
+                                <p>Total {total} </p>
+                                <button
+                                    onClick={cartCheckoutHandler}
+                                >Checkout</button>
+                            </div>
+                            <div className='cart-page-games-card-container'>
+                                {cart.length === 0 ?
+                                    <NavLink exact to='/games/browse'
+                                        style={{ textDecoration: 'none', fontSize: '3rem', border: '1px solid white', color: 'white', boxShadow: '5px 5px 5px gray' }}
+                                    >
+                                        Your cart is empty. Add games to your cart!
+                                    </NavLink>
+                                    :
+                                    cart.map(game => {
+                                        return <CartGamesCard
+                                            key={game.id}
+                                            game={game}
+                                            removeFromCartHandler={removeFromCartHandler}
+                                        />
+                                    })}
+                            </div>
+                        </div>
                     </div>
-                    <div className='cart-page-games-card-container'>
-                        {cart.length === 0 ?
-                            <NavLink exact to='/games/browse'
-                            style={{textDecoration: 'none', fontSize: '3rem', border: '1px solid white', color: 'white', boxShadow: '5px 5px 5px gray'}}
-                            >
-                                Your cart is empty. Add games to your cart!
-                            </NavLink>
-                            :
-                            cart.map(game => {
-                                return <CartGamesCard
-                                    key={game.id}
-                                    game={game}
-                                    removeFromCartHandler={removeFromCartHandler}
-                                    />
-                            })}
-                    </div>
-                </div>
-            </div>
+            }
+        </>
+
 
     )
 }
--- react-app/src/components/GamesPages/BrowseGamesPages.js
+++ react-app/src/components/GamesPages/BrowseGamesPages.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getAllCommentsThunk } from "../../store/comments"
 import { useHistory } from "react-router-dom"
@@ -7,6 +7,7 @@ import { getAllGamesThunk } from "../../store/games"
 import './BrowseGamesPages.css'
 import LowerNavBar from "../LowerNavBar/LowerNavBar"
 import Footer from "../UI/Footer"
+import LoadingScreen from '../UI/Loading/LoadingScreen'
 
 
 const BrowseGamesPages = () => {
@@ -14,9 +15,12 @@ const BrowseGamesPages = () => {
     const history = useHistory()
     const allGames = useSelector(state => state.games.allGames)
     const sessionUser = useSelector(state => state.session.user)
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        dispatch(getAllGamesThunk())
+        dispatch(getAllGamesThunk()).then(() => {
+            setLoading(false);
+        })
     }, [dispatch])
 
 
@@ -26,21 +30,27 @@ const BrowseGamesPages = () => {
     console.log('CONVERTED', convertedGames)
 
     return (
-        <>
-            <div className='browse-games-pages-outer'>
-                <div className='browse-games-pages-inner'>
-                    <LowerNavBar sessionUser={sessionUser} browse={true} />
-                    <div className='browse-games-pages-games'>
-                        {convertedGames && convertedGames.map(game => {
-                            // console.log('GAAAEM', game)
-                            return <BrowseGamesPagesCard
-                                key={game.id}
-                                game={game} />
-                        })}
+        <> {
+            loading ? <LoadingScreen /> :
+
+                <div>
+
+                    <div className='browse-games-pages-outer'>
+                        <div className='browse-games-pages-inner'>
+                            <LowerNavBar sessionUser={sessionUser} browse={true} />
+                            <div className='browse-games-pages-games'>
+                                {convertedGames && convertedGames.map(game => {
+                                    // console.log('GAAAEM', game)
+                                    return <BrowseGamesPagesCard
+                                        key={game.id}
+                                        game={game} />
+                                })}
+                            </div>
+                        </div>
                     </div>
+                    <Footer />
                 </div>
-            </div>
-            <Footer />
+        }
         </>
     )
 }
