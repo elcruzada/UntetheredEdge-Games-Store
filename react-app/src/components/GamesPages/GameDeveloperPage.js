40eb2d44672bb5a8febfabf0e0870f585de7c8ea
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-06-04 16:12:18 -0500

    finished portal

--- react-app/src/components/GamesPages/GameDeveloperPage.js
+++ react-app/src/components/GamesPages/GameDeveloperPage.js
@@ -85,9 +85,9 @@ const GameDeveloperPage = () => {
                         />
                         <div className="overlay-text">
                             <img
-                            src={UELogo}
-                            style={{height: '3rem', marginBottom: '2rem'}}
-                            alt='gamingLogo'
+                                src={UELogo}
+                                style={{ height: '3rem', marginBottom: '2rem' }}
+                                alt='gamingLogo'
                             ></img>
                             <h2 style={{ marginBottom: '2rem' }}>Publish with us!</h2>
                             {
@@ -136,20 +136,77 @@ const GameDeveloperPage = () => {
                             }
                         </div>
                     </div>
-                            <div class="developer-page-column-container">
-                                <div class="developer-page-column">
-                                    <div class="developer-column-item">Column 1, Div 1</div>
-                                    <div class="developer-column-item">Column 1, Div 2</div>
-                                </div>
-                                <div class="developer-page-column">
-                                    <div class="developer-column-item">Column 2, Div 1</div>
-                                    <div class="developer-column-item">Column 2, Div 2</div>
-                                </div>
-                                <div class="developer-page-column-column">
-                                    <div class="developer-column-item">Column 3, Div 1</div>
-                                    <div class="developer-column-item">Column 3, Div 2</div>
-                                </div>
+                    <div class="developer-page-column-container">
+                        <div class="developer-page-column">
+                            {/* <div className='developer-page-column-item-container'> */}
+                            <div class="developer-column-item">
+
+                               <i className="fa-solid fa-globe" style={{ fontSize: '3.5rem', color: '#C69749', marginBottom: '1rem'}}></i>
+                                <h2>Reach a global audience</h2>
+                                <p>
+                                Direct distribution to over 23 users across 1 country with 1 language supported.
+                                </p>
                             </div>
+                            {/* </div> */}
+                        </div>
+                        <div class="developer-page-column">
+                            <div class="developer-column-item">
+                                <i className="fa-solid fa-money-check-dollar" style={{ fontSize: '3.5rem', color: '#C69749', marginBottom: '1rem' }}></i>
+                            <h2>12%/88% Revenue Split</h2>
+                            <p>
+                            Keep 12% of the revenue and monitor product performance with an abacus.
+
+                            </p>
+
+                            </div>
+                        </div>
+                        <div class="developer-page-column">
+                            <div class="developer-column-item">
+                                <i className="fa-sharp fa-solid fa-gamepad" style={{ fontSize: '3.5rem', color: '#C69749', marginBottom: '1rem' }}></i>
+                            <h2>Drive Player Engagement</h2>
+                            <p>
+                            Tap into store features like comment trolls, empty wishlists, store-wide window-shopping and more!
+
+                            </p>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="developer-page-column-container">
+                        <div class="developer-page-column">
+                            {/* <div className='developer-page-column-item-container'> */}
+                            <div class="developer-column-item">
+                               <i className="fa-solid fa-cart-plus" style={{ fontSize: '3.5rem', color: '#C69749', marginBottom: '1rem' }}></i>
+                                <h2>Worldwide E-Commerce</h2>
+                                <p>
+                                UEI's payment service supports 1 payment method with no regional currencies.
+                                </p>
+                            </div>
+                            {/* </div> */}
+                        </div>
+                        <div class="developer-page-column">
+                            <div class="developer-column-item">
+                                <i className="fa-solid fa-wallet" style={{ fontSize: '3.5rem', color: '#C69749', marginBottom: '1rem' }}></i>
+                            <h2>UE Wallet</h2>
+                            <p>
+                            Users can load up their Wallet with imaginary funds to spend on products and services in the store.
+
+                            </p>
+
+                            </div>
+                        </div>
+                        <div class="developer-page-column">
+                            <div class="developer-column-item">
+                                <i className="fa-solid fa-person-circle-plus" style={{ fontSize: '3.5rem', color: '#C69749', marginBottom: '1rem' }}></i>
+                            <h2>Additional Benefits</h2>
+                            <p>
+                            Hard IARC ratings in the Developer Portal, request high-cost localization for store pages.
+
+                            </p>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer />
